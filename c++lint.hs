e29e9cbd96480f0e7532cbd6f61932554cd45f70
Bence Fabian <begnoc@gmail.com>
2017-10-10 17:35:01 +0200

    Command line arguments (stubs only)
    TODO:
    - implement includes/defines/checks
    - make them optional

--- c++lint.hs
+++ c++lint.hs
@@ -3,12 +3,44 @@
 import Turtle
 import System.Environment (lookupEnv)
 import Data.Text (pack, split)
+import Prelude hiding (FilePath)
+
+type Define = (Text, Text)
+
+data Settings = Settings
+  { file     :: FilePath
+  , checks   :: [Text]
+  , includes :: [Text]
+  , defines  :: [Define]
+  }
 
 getPaths :: String -> IO [Text]
 getPaths s = do
   e <- lookupEnv s
   return (maybe [] (\t -> split (':'==) (pack t)) e)
 
+parseFile :: Parser FilePath
+parseFile = argPath "file" empty
+
+parseChecks :: Parser [Text]
+parseChecks = opt (\t -> Just [t]) "checks" 'c'  empty
+
+parseIncludes :: Parser [Text]
+parseIncludes = opt (\t -> Just [t]) "includes" 'i'  empty
+
+parseDefines :: Parser [Define]
+parseDefines = opt (\t -> Just [(t, t)]) "defines" 'd'  empty
+
+parseSettings :: IO Settings
+parseSettings = options "C++ static linter" settingParser
+  where
+    settingParser :: Parser Settings
+    settingParser = Settings <$> parseFile
+                             <*> parseChecks
+                             <*> parseIncludes
+                             <*> parseDefines
+
 main = do
+  (Settings file checks includes defines) <- parseSettings
   dirs <- getPaths "CPPLINT_INCLUDE_DIRS"
-  print dirs
+  print file
